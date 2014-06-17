ea3984f7872a315fe6b787c3ca43082007849ac6
atomfrede <atomfrede@gmail.com>
2014-06-17 18:37:00 +0200

    Fix #19
    Adapted parser for new webpage structure.

--- mensa-ubi/src/de/atomfrede/android/mensa/ubi/data/Parser.java
+++ mensa-ubi/src/de/atomfrede/android/mensa/ubi/data/Parser.java
@@ -33,9 +33,9 @@ public class Parser {
 	public static WeeklyMenu parseMenu(boolean reload, String data, SharedPreferences settings, String url, String xmlKey) throws Exception {
 		WeeklyMenu mensaMenu = new WeeklyMenu();
 		Document doc;
-		if (data != null && !data.equals("") && !reload){
+		if (data != null && !data.equals("") && !reload) {
 			doc = Jsoup.parse(data);
-		}else {
+		} else {
 			doc = Jsoup.connect(url).get();
 			String xmlData = doc.toString();
 			settings.edit().putString(xmlKey, xmlData).commit();
@@ -67,24 +67,30 @@ public class Parser {
 
 			Elements importantElements = row.select("span.important");
 			int impCounter = 0;
-			if(!importantElements.isEmpty()){
+			if (!importantElements.isEmpty()) {
 				StringBuilder sb = new StringBuilder();
-				for(Element important:importantElements){
-					if(impCounter == 0)
-						sb.append(important.text()+"\n\n");
-					else if(impCounter == 1)
-						sb.append(important.text()+" ");
-					else 
+				for (Element important : importantElements) {
+					if (impCounter == 0)
+						sb.append(important.text() + "\n\n");
+					else if (impCounter == 1)
+						sb.append(important.text() + " ");
+					else
 						sb.append(important.text());
 					impCounter++;
 				}
 				menu.setText(sb.toString());
 				menu.setPrice("");
-			}else {
-				String menuText = row.child(1).child(0).text();
+			} else {
+				// String menuText = row.child(1).child(0).text();
+				String menuText = row.child(1).text();
+
+				if (menuText.trim().endsWith("€")) {
+					menuText = menuText.trim().substring(0, menuText.length() - 6);
+				}
+
 				menu.setText(menuText);
 
-				Elements priceElements = row.child(2).select("p");
+				Elements priceElements = row.child(3).select("p");
 				StringBuilder sb = new StringBuilder();
 				int counter = 0;
 				for (Element priceElement : priceElements) {
@@ -93,9 +99,15 @@ public class Parser {
 					if (counter != priceElements.size())
 						sb.append("\n");
 				}
+
 				menu.setPrice(sb.toString());
+
+				if (priceElements.size() == 0) {
+					menu.setPrice(row.child(3).text());
+				}
+
 			}
-			
+
 			dailyMenu.addMenu(menu);
 		}
 	}
